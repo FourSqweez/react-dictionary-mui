bb0ab305c4bf6811d73fe91131d8b730c5756bd4
FourSqweez <four7518@gmail.com>
2021-05-19 16:00:19 +0700

    ADD : get data from dictionary api, design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import { Container } from '@material-ui/core'
+import Header from './components/Header'
+import styled from 'styled-components'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [word, setWord] = useState('')
+	const [meanings, setMeanings] = useState([])
+	const [category, setCategory] = useState('en')
+
+	const fetchDictionary = async () => {
+		try {
+			const { data } = await axios.get(
+				`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+			)
+			setMeanings(data)
+			console.log(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchDictionary()
+	}, [word, category])
+	
+	return (
+		<AppContainer>
+			<ContentContainer maxWidth="md">
+				<Header
+					word={word}
+					setWord={setWord}
+					category={category}
+					setCategory={setCategory}
+				/>
+			</ContentContainer>
+		</AppContainer>
+	)
 }
 
-export default App;
+export default App
+
+const AppContainer = styled.div`
+	height: 100vh;
+	background-color: #282c34;
+	color: white;
+`
+const ContentContainer = styled(Container)`
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+`
