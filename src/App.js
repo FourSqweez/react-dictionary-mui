e3d7805c60736b9cce9864ab7ce15f842b5094fd
FourSqweez <four7518@gmail.com>
2021-05-19 23:58:26 +0700

    ADD : Theme (dark, light mode) using Swich from mui

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Container } from '@material-ui/core'
+import { Container, Switch, withStyles } from '@material-ui/core'
 import Header from './components/Header'
 import styled from 'styled-components'
+import Definitions from './components/Definitions'
+import { grey } from '@material-ui/core/colors'
+
+const DarkMode = withStyles({
+	switchBase: {
+		color: grey[300],
+		'&$checked': {
+			color: grey[500],
+		},
+		'&$checked + $track': {
+			backgroundColor: grey[500],
+		},
+	},
+	checked: {},
+	track: {},
+})(Switch)
 
 function App() {
 	const [word, setWord] = useState('')
 	const [meanings, setMeanings] = useState([])
 	const [category, setCategory] = useState('en')
+	const [darkMode, setDarkMode] = useState(true)
+	const [apiError, setApiError] = useState(false)
 
 	const fetchDictionary = async () => {
 		try {
 			const { data } = await axios.get(
 				`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
 			)
+			setApiError(false)
 			setMeanings(data)
-			console.log(data)
+			//console.log(data)
 		} catch (error) {
-			console.log(error)
+			setApiError(true)
 		}
 	}
 
 	useEffect(() => {
 		fetchDictionary()
 	}, [word, category])
-	
+
 	return (
-		<AppContainer>
+		<AppContainer darkMode={darkMode}>
+			<DarkModeContainer>
+				<span>{darkMode ? 'Dark Mode' : 'Light Mode'}</span>
+				<DarkMode
+					checked={darkMode}
+					onChange={() => setDarkMode(!darkMode)}
+				/>
+			</DarkModeContainer>
 			<ContentContainer maxWidth="md">
 				<Header
 					word={word}
@@ -35,6 +61,15 @@ function App() {
 					category={category}
 					setCategory={setCategory}
 				/>
+
+				{meanings && (
+					<Definitions
+						word={word}
+						category={category}
+						meanings={meanings}
+						apiError={apiError}
+					/>
+				)}
 			</ContentContainer>
 		</AppContainer>
 	)
@@ -44,11 +79,21 @@ export default App
 
 const AppContainer = styled.div`
 	height: 100vh;
-	background-color: #282c34;
-	color: white;
+	background-color: ${({ darkMode }) => (darkMode ? '#282c34' : '#fff')};
+	color: ${({ darkMode }) => (darkMode ? 'white' : 'black')};
+	transition: all 0.5s linear;
 `
 const ContentContainer = styled(Container)`
-	display: flex;
-	flex-direction: column;
-	height: 100vh;
+	&& {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		justify-content: space-evenly;
+	}
+`
+
+const DarkModeContainer = styled.div`
+	position: absolute;
+	top: 0;
+	right: 15px;
 `
